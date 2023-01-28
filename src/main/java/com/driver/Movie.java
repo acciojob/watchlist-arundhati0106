d9aff4eb26ced94a6cae9b1169e2a3eb16855a16
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-28 14:21:42 +0530

    All classes made

---
+++ src/main/java/com/driver/Movie.java
@@ -0,0 +1,25 @@
+package com.driver;
+
+public class Movie {
+    //attributes
+    private String name;
+    private int duration;
+    private double imdbRating;
+
+    //constructor
+    public Movie(String name, int duration, double imdbRating) {
+        this.name = name;
+        this.duration = duration;
+        this.imdbRating = imdbRating;
+    }
+
+    //getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
+
+    public double getImdbRating() { return imdbRating; }
+    public void setImdbRating(double imdbRating) { this.imdbRating = imdbRating; }
+}
