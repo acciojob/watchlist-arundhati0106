94523f3675066f9bd701771ad8746315a239505a
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-28 14:38:34 +0530

    change variable name, acc to test cases

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Movie {
     //attributes
     private String name;
-    private int duration;
+    private int durationInMinutes;
     private double imdbRating;
 
     //constructor
-    public Movie(String name, int duration, double imdbRating) {
+    public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
-        this.duration = duration;
+        this.durationInMinutes = durationInMinutes;
         this.imdbRating = imdbRating;
     }
 
@@ -17,8 +17,8 @@ public class Movie {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public int getDuration() { return duration; }
-    public void setDuration(int duration) { this.duration = duration; }
+    public int getdurationInMinutes() { return durationInMinutes; }
+    public void setdurationInMinutes(int durationInMinutes) { this.durationInMinutes = durationInMinutes; }
 
     public double getImdbRating() { return imdbRating; }
     public void setImdbRating(double imdbRating) { this.imdbRating = imdbRating; }
