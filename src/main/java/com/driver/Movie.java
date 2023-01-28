f4f67201bde70f854a466fe8b563e72401bf7264
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-28 14:43:37 +0530

    change variable name

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -17,8 +17,8 @@ public class Movie {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public int getdurationInMinutes() { return durationInMinutes; }
-    public void setdurationInMinutes(int durationInMinutes) { this.durationInMinutes = durationInMinutes; }
+    public int getDurationInMinutes() { return durationInMinutes; }
+    public void setDurationInMinutes(int durationInMinutes) { this.durationInMinutes = durationInMinutes; }
 
     public double getImdbRating() { return imdbRating; }
     public void setImdbRating(double imdbRating) { this.imdbRating = imdbRating; }
